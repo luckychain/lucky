bd4c3022c5da597f5e158f58e5f006c7d1028138
Howard Wu <howardwu@berkeley.edu>
2016-10-16 17:06:03 -0700

    merge peer back

--- src/js/core-app.js
+++ src/js/core-app.js
@@ -132,7 +132,6 @@ var coreApp = function (options) {
     ipfs.id().then((id) => {
       /* IPFS daemon id */
       IPFS_ID = id.ID;
-      Peer = require('./peers')(ipfs, logger, validObject, IPFS_ID, DIRECTORY, ID_DIRECTORY);
       console.log("IPFS_ID: " + IPFS_ID);
 
       /* Load Pubsub */
@@ -268,6 +267,109 @@ var coreApp = function (options) {
       else if (i === array.length - 1) return false;
     }
   }
+
+/********************************** PEERS ************************************/
+
+/* Returns the hash identifier for this blockchain application */
+  function ipfsPeerID() {
+    logger("ipfsPeerID");
+    return new Promise((resolve) => {
+      ipfs.add(ID_DIRECTORY, (err, res) => {
+        if (err) logger("error: ipfsPeerID failed", err);
+        else {
+          var hash = res[0].Hash;
+          logger("ipfsPeerID: " + hash);
+          resolve(hash);
+        }
+      });
+    });
+  };
+
+  /* Returns the peers who are a part of this blockchain application */
+  function ipfsPeerDiscovery(hash) {
+    logger("ipfsPeerDiscovery");
+    return new Promise((resolve) => {
+      oboe("http://127.0.0.1:5001/api/v0/dht/findprovs\?arg\=" + hash).done((res) => {
+        if (res.Type === 4) {
+          var id = res.Responses[0].ID;
+          if (id !== IPFS_ID) {
+            ipfsPubSub(id);
+            logger("ipfsPeerDiscovery: " + id);
+          }
+        }
+      }).fail(function() {
+        console.log("error: ipfsPeerDiscovery failed to find peers");
+      });
+    });
+  };
+
+  function ipfsPubSub(peerID) {
+    logger("ipfsPubSub");
+    ipfsPeerResolve(peerID).then((path) => { return ipfsGetData(path, "/pubsub"); }).then((p2pID) => {
+      console.log("Dialing " + p2pID.id);
+      var id = PeerId.createFromJSON(p2pID);
+      var peer = new PeerInfo(id);
+      peer.multiaddr.add(multiaddr('/ip4/0.0.0.0/tcp/10333'));
+      pubSub.connect(peerPublisher);
+    });
+  };
+
+  /* Publish the files under DIRECTORY using IPNS */
+  function ipfsPeerPublish() {
+    logger("ipfsPeerPublish");
+    return new Promise((resolve) => {
+      ipfs.add(DIRECTORY, { recursive: true }, (err, addRes) => {
+        if (err) logger("error: ipfsPeerPublish failed", err);
+        else {
+          var hash = addRes.filter((path) => { return path.Name === DIRECTORY; })[0].Hash;
+          ipfs.name.publish(hash, null, (err, publishRes) => {
+            if (err) logger("ipfsPeerPublish error: ipfs.name.publish failed", err);
+            else {
+              var name = publishRes.Name;
+              logger("ipfsPeerPublish successful: " + name);
+              resolve(name);
+            }
+          });
+        }
+      });
+    });
+  };
+
+  /* Returns the resolved path given a peer id - called every pubsub interval */
+  function ipfsPeerResolve(id) {
+    logger("ipfsPeerResolve");
+    return new Promise((resolve) => {
+      ipfs.name.resolve(id, null, (err, nameRes) => {
+        if (err) {
+          peers = _.without(peers, id);
+          logger("ipfsPeerResolve error: ipfs.name.resolve failed for " + id, err);
+        } else resolve(nameRes.Path);
+      });
+    });
+  };
+
+  /* Returns the requested data given a resolved IPFS path and link */
+  function ipfsGetData(path, link) {
+    logger("ipfsGetData");
+    return new Promise((resolve) => {
+      ipfs.cat(path + link, (err, catRes) => {
+        if (err) logger("ipfsGetData error: ipfs.cat failed", err);
+        else {
+          var chunks = [];
+          catRes.on("data", (chunk) => { chunks.push(chunk); });
+          catRes.on("end", () => {
+            if (chunks.length > 0) {
+              var data = chunks.join("");
+              if (validObject(data)) {
+                if (typeof data === "string") data = JSON.parse(data);
+                resolve(data);
+              }
+            }
+          })
+        }
+      })
+    })
+  };
 
 /*********************************** IPFS ************************************/
 
