3c2a43c09e96fc92635812672600cd5d5427ea9e
Mitar <mitar.git@tnode.com>
2017-03-06 18:34:49 -0800

    Removing bold from markup.

--- src/jsx/components/Footer.js
+++ src/jsx/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                 <Grid>
                     <Row>
                         <Col sm={6}>
-                            <p>Powered by <strong><a href="https://software.intel.com/en-us/sgx">Intel SGX</a></strong>, <strong><a href="https://ipfs.io/">IPFS</a></strong>, <strong><a href="https://socket.io/">Socket.io</a></strong> and <strong><a href="https://facebook.github.io/react/">React</a></strong> with <strong><a href="https://facebook.github.io/flux/">Flux</a></strong> architecture.</p>
+                            <p>Powered by <a href="https://software.intel.com/en-us/sgx">Intel SGX</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://socket.io/">Socket.io</a> and <a href="https://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/flux/">Flux</a> architecture.</p>
                         </Col>
                         <Col sm={6}>
                             <p><a href="https://github.com/luckychain/lucky">Source code</a>.</p>
