3390ff8197a497736a2534dce9939cede505269d
Mitar <mitar.git@tnode.com>
2017-03-06 20:37:16 -0800

    Footer.

--- src/jsx/components/Footer.js
+++ src/jsx/components/Footer.js
@@ -8,9 +8,9 @@ class Footer extends React.Component {
         <Grid>
           <Row>
             <Col sm={6}>
-              <p>Powered by <a href="https://software.intel.com/en-us/sgx">Intel SGX</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://socket.io/">Socket.io</a> and <a href="https://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/flux/">Flux</a> architecture.</p>
+              <p>Powered by <a href="https://software.intel.com/en-us/sgx">Intel SGX</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://socket.io/">Socket.io</a>, and <a href="https://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/flux/">Flux</a> architecture.</p>
             </Col>
-            <Col sm={6}>
+            <Col sm={6} className="text-right">
               <p><a href="https://github.com/luckychain/lucky">Source code</a>.</p>
             </Col>
           </Row>
