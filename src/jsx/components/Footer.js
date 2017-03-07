d7afa2ea16b30c51d64c281109ea3d88b2e1b5fd
Mitar <mitar.git@tnode.com>
2017-03-07 01:41:50 -0800

    Resizing footer columns.

--- src/jsx/components/Footer.js
+++ src/jsx/components/Footer.js
@@ -7,10 +7,10 @@ class Footer extends React.Component {
       <footer>
         <Grid>
           <Row>
-            <Col sm={6}>
+            <Col sm={10}>
               <p>Powered by <a href="https://software.intel.com/en-us/sgx">Intel SGX</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://socket.io/">Socket.io</a>, and <a href="https://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/flux/">Flux</a> architecture.</p>
             </Col>
-            <Col sm={6} className="text-right">
+            <Col sm={2} className="text-right">
               <p><a href="https://github.com/luckychain/lucky">Source code</a>.</p>
             </Col>
           </Row>
