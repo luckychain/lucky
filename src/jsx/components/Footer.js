956ce7e4c87d976488e99d2c344dd6f711002b08
Mitar <mitar.git@tnode.com>
2017-03-16 01:37:09 -0700

    Fixing footer.

--- src/jsx/components/Footer.js
+++ src/jsx/components/Footer.js
@@ -7,10 +7,10 @@ class Footer extends React.Component {
       <footer>
         <Grid>
           <Row>
-            <Col sm={10}>
+            <Col sm={8}>
               <p>Powered by <a href="https://software.intel.com/en-us/sgx">Intel SGX</a>, <a href="https://ipfs.io/">IPFS</a>, <a href="https://socket.io/">Socket.io</a>, and <a href="https://facebook.github.io/react/">React</a> with <a href="https://facebook.github.io/flux/">Flux</a> architecture.</p>
             </Col>
-            <Col sm={2} className="text-right">
+            <Col sm={4} className="text-right">
               <p><a href="https://github.com/luckychain/lucky">Source code</a>. <a href="https://github.com/luckychain/lucky#whitepaper">Whitepaper</a>.</p>
             </Col>
           </Row>
